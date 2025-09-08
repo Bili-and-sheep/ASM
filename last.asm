702a91a4e0c90e1157055374d52a04a499a7741b
Bili-and-sheep <eliottdu34@gmail.com>
2025-09-08 16:31:02 +0200

    Add print and exit syscalls in last.asm

---
+++ last.asm
@@ -0,0 +1,24 @@
+section .text
+    global _start
+
+print:
+    push rbp        ;prologue
+    mov rbp, rsp
+
+    mov rax, 1      ; syscall: write
+    mov rdi, 1      ; file descriptor: stdout
+    push 0x30      ; push '0' character
+    mov rsi, rsp    ; pointer to buffer
+    mov rdx, 8      ; number of bytes
+    syscall
+    
+    leave           ;epilogue
+    ret
+
+exit:
+    ; push rbp
+    ; mov rbp, rsp
+
+    mov rax, 60     ; syscall: exit
+    mov rdi, 0      ; status: 0
+    syscall

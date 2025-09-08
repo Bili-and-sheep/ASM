d0e4a233fa4b09d26867833f3f8a37439a29b8b9
Bili-and-sheep <eliottdu34@gmail.com>
2025-09-08 12:44:57 +0200

    . ?????

--- loop.s
+++ loop.s
@@ -5,7 +5,7 @@ _start:
     mov rax, 4
     add rax, 2
 
-    add rax, 30h.  ;30h = 0 en ascii , on ajoute 30h pour convertir le nombre en ascii, ex : 6 + 30h = 36h = 6 en ascii
+    add rax, 30h  ;30h = 0 en ascii , on ajoute 30h pour convertir le nombre en ascii, ex : 6 + 30h = 36h = 6 en ascii
     push rax
 
 loop :

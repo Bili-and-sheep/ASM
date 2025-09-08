c4e0a8be8bb26d06a0dbbaef05658d8588f4ab83
Bili-and-sheep <eliottdu34@gmail.com>
2025-09-08 10:43:12 +0200

    Implement output and exit syscalls in exo1_correction.s

--- exo1_correction.s
+++ exo1_correction.s
@@ -8,5 +8,14 @@ _start:
     add rax, 30h.  ;30h = 0 en ascii , on ajoute 30h pour convertir le nombre en ascii, ex : 6 + 30h = 36h = 6 en ascii
     push rax
 
-    
+    mov rax, 1 ;write
+    mov rdi, 1 ;stdout
+    mov rsi, rsp ;adresse du buffer
+    mov rdx, 1 ;taille du buffer en octets
+    syscall
+
+    mov rdi, 60 ;exit
+    mov rsi, 0 ;code de retour
+    syscall
+
 
